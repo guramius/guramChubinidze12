c500b2ac33a6dbf28483cecb492af039219dfcf6
guramius <96048349+guramius@users.noreply.github.com>
2024-10-30 19:22:57 +0400

    new porject

---
+++ first project/src/assets/components/Header.jsx
@@ -0,0 +1,16 @@
+
+const Header = () => {
+  return (
+    <div><svg width="240" height="38" viewBox="0 0 240 38" fill="none" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+    <rect id="color_logo_transparent" width="240" height="38" fill="url(#pattern0_0_472)"/>
+    <defs>
+    <pattern id="pattern0_0_472" patternContentUnits="objectBoundingBox" width="1" height="1">
+    <use xlinkHref="#image0_0_472" transform="scale(0.000342114 0.0021322)"/>
+    </pattern>
+    </defs>
+    </svg>
+    </div>
+  )
+}
+
+export default Header;
