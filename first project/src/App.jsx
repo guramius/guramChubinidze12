80abe36fb02431cca503388702a391c7a601e505
guramius <96048349+guramius@users.noreply.github.com>
2024-11-04 17:08:59 +0400

    fix it

--- first project/src/App.jsx
+++ first project/src/App.jsx
@@ -1,5 +1,5 @@
 import Header from "./assets/components/Header";
-import Content from "./assets/components/content";
+import Content from "./assets/components/Content";
 import AbautUs from "./assets/components/AbautUs";
 import Community from "./assets/components/Community";
 import Footer from "./assets/components/Footer";
